0b220a1e6c02abd44162704aa4f08049df2a00f2
karine <karine.samyn@hogent.be>
2017-09-05 18:25:56 +0200

    Add project files.

---
+++ HelloVisualStudio/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SayHello());
+            Console.ReadLine();
+        }
+
+        static string SayHello()
+        {
+            return "Hello C#";
+        }
+    }
+}
